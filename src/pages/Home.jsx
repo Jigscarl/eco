6c9dbe7629619d559bcb744fee70c4f93b69ff10
Jigscarl <eboko2022@gmail.com>
2026-03-07 04:16:04 -0800

    dis

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,13 @@
 export default function Home(){
     return(
         <>
-        <div>
-            <h1 className="text-xl md:text-2xl lg:text-4xl text-center font-semibold text-green-500">
+        <div class="bg-white ">
+            <h1 className="text-2xl md:text-2xl lg:text-4xl text-center font-semibold text-green-500">
                 Eco track
             </h1>
             <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus tempore, exercitationem expedita maxime in veritatis aspernatur quae perspiciatis error minima ea maiores illum necessitatibus culpa tenetur non totam reprehenderit atque beatae voluptas nam deserunt repellendus. Libero autem magni facere harum esse, nisi, omnis molestiae debitis cumque perspiciatis dolores eveniet quidem!</p>
+                <p class="p-2 text center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus tempore, exercitationem expedita maxime in veritatis aspernatur quae perspiciatis error minima ea maiores illum necessitatibus culpa tenetur non totam reprehenderit atque beatae voluptas nam deserunt repellendus. Libero autem magni facere harum esse, nisi, omnis molestiae debitis cumque perspiciatis dolores eveniet quidem!</p>
+                <h2 className="text-red-500 text-4xl md:text-5xl lg:text-6xl">Disclaimer :: I REPEAT, I REPEAT THIS IS A TEST</h2>
             </div>
         </div>
         </>
