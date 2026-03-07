781d04a30342086e16b5775fd1b5bcf76558d02c
Jigscarl <eboko2022@gmail.com>
2026-03-07 04:08:52 -0800

    cd

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-export default function Main() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home(){
+    return(
+        <>
+        <div>
+            <h1 className="text-xl md:text-2xl lg:text-4xl text-center font-semibold text-green-500">
+                Eco track
+            </h1>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus tempore, exercitationem expedita maxime in veritatis aspernatur quae perspiciatis error minima ea maiores illum necessitatibus culpa tenetur non totam reprehenderit atque beatae voluptas nam deserunt repellendus. Libero autem magni facere harum esse, nisi, omnis molestiae debitis cumque perspiciatis dolores eveniet quidem!</p>
+            </div>
+        </div>
+        </>
+    )
+}
